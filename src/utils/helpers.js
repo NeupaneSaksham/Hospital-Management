2b2060713c4f3d92eb97d89173d2f444384d8a36
NeupaneSaksham <shakshamNeupane@gmail.com>
2025-07-05 12:11:06 +0545

    Add helper utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,5 @@ export const helperFunc_665 = () => console.log('Helper helperFunc_665 executed'
 export const helperFunc_656 = () => console.log('Helper helperFunc_656 executed');
 
 export const helperFunc_943 = () => console.log('Helper helperFunc_943 executed');
+
+export const helperFunc_581 = () => console.log('Helper helperFunc_581 executed');
