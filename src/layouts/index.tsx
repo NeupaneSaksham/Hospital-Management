77dcdffe355c0b0368304093bfd0be7ac88fef25
NeupaneSaksham <shakshamNeupane@gmail.com>
2025-08-19 12:27:00 +0545

    Setup layouts

---
+++ src/layouts/index.tsx
@@ -0,0 +1,5 @@
+// Main layouts for different user roles
+export { default as DoctorLayout } from '../features/doctor/layout'
+
+// Root layout for the entire application
+export { default as RootLayout } from './RootLayout'
