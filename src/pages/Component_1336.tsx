7c2bedb8ebfd6ccaa4e1cfeb7919bc5e82464a06
NeupaneSaksham <shakshamNeupane@gmail.com>
2025-08-26 20:30:13 +0545

    Add new page Component_1336

---
+++ src/pages/Component_1336.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Component_1336 = () => {
+    return (
+        <div className="page-container">
+            <h1>Component_1336</h1>
+            <p>Content for Component_1336</p>
+        </div>
+    );
+};
+
+export default Component_1336;
